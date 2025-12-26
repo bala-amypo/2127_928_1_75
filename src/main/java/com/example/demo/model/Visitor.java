a82eb191ba48f679e62964048a90defd205e3c12
bala-amypo <bala@amypo.in>
2025-12-26 06:28:41 +0000

    User commit

--- src/main/java/com/example/demo/model/Visitor.java
+++ src/main/java/com/example/demo/model/Visitor.java
@@ -1,21 +1,14 @@
 package com.example.demo.model;
 
-import jakarta.persistence.*;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-@Entity
 @Data
-@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class Visitor {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    private String fullName;
+    private String name;
     private String phone;
-    private String email;
-    private String idProof;
 }
