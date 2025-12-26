52ad1cd81e9190a96fcf4eff7cca7d569b2c1e71
bala-amypo <bala@amypo.in>
2025-12-26 13:25:47 +0000

    User commit

--- src/main/java/com/example/demo/repository/ScoreAuditLogRepository.java
+++ src/main/java/com/example/demo/repository/ScoreAuditLogRepository.java
@@ -3,5 +3,9 @@ package com.example.demo.repository;
 import com.example.demo.model.ScoreAuditLog;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ScoreAuditLogRepository extends JpaRepository<ScoreAuditLog, Long> {
+
+    List<ScoreAuditLog> findByUserId(Long userId);
 }
--- src/main/java/com/example/demo/repository/VisitLogRepository.java
+++ src/main/java/com/example/demo/repository/VisitLogRepository.java
@@ -3,5 +3,9 @@ package com.example.demo.repository;
 import com.example.demo.model.VisitLog;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface VisitLogRepository extends JpaRepository<VisitLog, Long> {
+
+    List<VisitLog> findByUserId(Long userId);
 }
