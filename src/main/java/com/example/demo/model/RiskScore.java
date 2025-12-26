95548897120cedbef8739abba9106d78127c9b78
bala-amypo <bala@amypo.in>
2025-12-26 15:18:11 +0000

    User commit

--- src/main/java/com/example/demo/model/RiskScore.java
+++ src/main/java/com/example/demo/model/RiskScore.java
@@ -1,14 +1,22 @@
 package com.example.demo.model;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@Entity
+@Table(name = "risk_scores")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class RiskScore {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
     private String level;
+
     private int score;
 }
--- src/main/java/com/example/demo/repository/RiskScoreRepository.java
+++ src/main/java/com/example/demo/repository/RiskScoreRepository.java
@@ -1,7 +1,9 @@
 package com.example.demo.repository;
 
-import com.example.demo.model.RiskScoreModel;
+import com.example.demo.model.RiskScore;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface RiskScoreRepository extends JpaRepository<RiskScoreModel, Long> {
+@Repository
+public interface RiskScoreRepository extends JpaRepository<RiskScore, Long> {
 }
