a6bdedcb511ce6e98812ba27ffcd9e4a7cc378b0
bala-amypo <bala@amypo.in>
2025-12-28 07:42:49 +0000

    User commit

--- src/main/java/com/example/demo/repository/ScoreAuditLogRepository.java
+++ src/main/java/com/example/demo/repository/ScoreAuditLogRepository.java
@@ -1,7 +1,9 @@
 package com.example.demo.repository;
 
-import com.example.demo.entity.ScoreAuditLog;
+import com.example.demo.model.ScoreAuditLog;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ScoreAuditLogRepository extends JpaRepository<ScoreAuditLog, Long> {
 }
